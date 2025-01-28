2feae0ed87117cc42e48abc52cec0dcb32689de6
Robert Fasciano <rob.fasciano@yahoo.com>
2025-01-28 08:30:18 -0500

    Through chapter 9 (no challenges)

--- CatNap/GameScene.swift
+++ CatNap/GameScene.swift
@@ -20,9 +20,15 @@ struct PhysicsCategory {
     static let Cat: UInt32 = 0b1 // 1
     static let Block: UInt32 = 0b10 // 2
     static let Bed: UInt32 = 0b100 // 4
+    static let Edge: UInt32 = 0b1000 // 8
+    static let Label: UInt32 = 0b10000 // 16
 }
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    var bedNode: BedNode!
+    var catNode: CatNode!
+    var playable = true
+
     override func didMove(to view: SKView) {
         // Calculate playable margin
         let maxAspectRatio: CGFloat = 16.0/9.0
@@ -32,13 +38,12 @@ class GameScene: SKScene {
         let playableRect = CGRect(x: 0, y: playableMargin,
                                   width: size.width, height: size.height-playableMargin*2)
         physicsBody = SKPhysicsBody(edgeLoopFrom: playableRect)
+        physicsWorld.contactDelegate = self
+        physicsBody!.categoryBitMask = PhysicsCategory.Edge
         
-        var bedNode: BedNode!
         bedNode = childNode(withName: "bed") as? BedNode
-        
-        var catNode: CatNode!
         catNode = childNode(withName: "//cat_body") as? CatNode
-        
+                
         //adding this is a work around for a frozen animation in scene
         self.isPaused = true
         self.isPaused = false
@@ -55,4 +60,54 @@ class GameScene: SKScene {
 //        bedNode.setScale(1.5)
 //        catNode.setScale(1.5)
     }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        if !playable {
+            return
+        }
+        let collision = contact.bodyA.categoryBitMask
+        | contact.bodyB.categoryBitMask
+        if collision == PhysicsCategory.Cat | PhysicsCategory.Bed {
+            print("SUCCESS")
+            win()
+        } else if collision == PhysicsCategory.Cat
+                    | PhysicsCategory.Edge {
+            print("FAIL")
+            lose()
+        }
+    }
+    
+    func inGameMessage(text: String) {
+        let message = MessageNode(message: text)
+        message.position = CGPoint(x: frame.midX, y: frame.midY)
+        addChild(message)
+    }
+    
+    func newGame() {
+        let scene = GameScene(fileNamed:"GameScene")
+        scene!.scaleMode = scaleMode
+        view!.presentScene(scene)
+    }
+    
+    func lose() {
+        playable = false
+        //1
+        SKTAudio.sharedInstance().pauseBackgroundMusic()
+        SKTAudio.sharedInstance().playSoundEffect("lose.mp3")
+        //2
+        inGameMessage(text: "Try again...")
+        //3
+        run(SKAction.afterDelay(5, runBlock: newGame))
+        catNode.wakeUp()
+    }
+    
+    func win() {
+        playable = false
+        SKTAudio.sharedInstance().pauseBackgroundMusic()
+        SKTAudio.sharedInstance().playSoundEffect("win.mp3")
+        inGameMessage(text: "Nice job!")
+        run(SKAction.afterDelay(3, runBlock: newGame))
+        catNode.curlAt(scenePoint: bedNode.position)
+    }
+    
 }
